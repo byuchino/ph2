3cea42c69819ad65256eeda9cc599a91dc094663
admin <root@localhost>
2022-04-29 20:22:11 +0000

    Lab redo

--- Investigation Lab.py
+++ Investigation Lab.py
@@ -11,9 +11,42 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'locate_source' block
+    locate_source(container=container)
 
     return
+
+def locate_source(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("locate_source() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.sourceAddress","artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'locate_source' call
+    for container_artifact_item in container_artifact_data:
+        if container_artifact_item[0] is not None:
+            parameters.append({
+                "ip": container_artifact_item[0],
+                "context": {'artifact_id': container_artifact_item[1]},
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("geolocate ip", parameters=parameters, name="locate_source", assets=["maxmind"])
+
+    return
+
 
 def on_finish(container, summary):
     phantom.debug("on_finish() called")
